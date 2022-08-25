da15759b78a2ff7ccaea3a783f361537cae206f8
Gioliano Bertoni <giobertoni1@gmail.com>
2022-08-25 20:16:04 -0300

    spring web com h2

---
+++ src/test/java/br/edu/infnet/springweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
